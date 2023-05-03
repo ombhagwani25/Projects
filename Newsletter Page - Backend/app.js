0909d68b8141382c56f9e376e02b1cd2416af680
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-03 21:02:19 +0530

    made working of form data POSTING

--- Newsletter Page - Backend/app.js
+++ Newsletter Page - Backend/app.js
@@ -2,11 +2,24 @@ const express = require("express")
 const request = require("request");
 const app = express();
 
+app.use(express.static("public"));
 
 const bodyParser = require("body-parser");
 
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+app.post("/", (req,res)=> {
+    let firstname  = req.body.firstname;
+    let lastname = req.body.lastname;
+    let email = req.body.email;
+    console.log("first name : " , firstname , "  last name :  " ,lastname , "  email : " , email);
+})
+
 app.get("/", (req,res)=> {
-    res.send("Server is working fine ...")
+    res.sendFile(__dirname + "/signup.html");
+
 
 })
 
