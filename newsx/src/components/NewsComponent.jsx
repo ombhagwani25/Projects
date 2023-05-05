fc60560827ecede3f6178a0d2f75ccaac7e3bca2
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-05 17:45:18 +0530

    fetching data from JSON

--- newsx/src/components/NewsComponent.jsx
+++ newsx/src/components/NewsComponent.jsx
@@ -1,42 +1,27 @@
 import React from 'react'
 import NewsItem from './NewsItem'
+import sampleOutput from  "../sampleOutput.json";
+
+
 
 export default function NewsComponent() {
 
     let imageUrl = "https://static.toiimg.com/thumb/msid-98646739,width-1070,height-580,imgsize-44954,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg";
-    return (
+    let json = sampleOutput.map((data) => (
+        <NewsItem  title={data.title} description={data.description} imageUrl={data.imageUrl} />
+    ));
+    
+    
+
+    return (        
         <>
-
-            {/* <NewsItem title="cricket news" description="sample description"  imageUrl={imageUrl}/> */}
-
-            <div className="container">
-
-                <h4 className="display-4">NewX - Top Headlines</h4>
-                <div className="row">
-                    <div className="col col-md4">
-                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
-                    </div>
-                    <div className="col col-md4">
-                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
-                    </div>
-                    <div className="col col-md4">
-                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col col-md4">
-                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
-                    </div>
-                    <div className="col col-md4">
-                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
-                    </div>
-                    <div className="col col-md4">
-                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
-                    </div>
-                </div>
-              
-
+         
+        <div className="container">
+            <h6 className="display-6">NewX - Top Headlines</h6>
+            <div className="row">
+                {json}
             </div>
+        </div>
         </>
     )
 }
