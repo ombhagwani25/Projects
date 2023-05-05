cb659167555c11e8017fb3de1255cf723941da92
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-05 13:04:17 +0530

    made components News Items

--- newsx/src/components/NewsComponent.jsx
+++ newsx/src/components/NewsComponent.jsx
@@ -2,11 +2,41 @@ import React from 'react'
 import NewsItem from './NewsItem'
 
 export default function NewsComponent() {
-  return (
-    <>
-    this is a news component...
-    <NewsItem title="cricket news" description="sample description" />
 
-    </>
-  )
+    let imageUrl = "https://static.toiimg.com/thumb/msid-98646739,width-1070,height-580,imgsize-44954,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg";
+    return (
+        <>
+
+            {/* <NewsItem title="cricket news" description="sample description"  imageUrl={imageUrl}/> */}
+
+            <div className="container">
+
+                <h4 className="display-4">NewX - Top Headlines</h4>
+                <div className="row">
+                    <div className="col col-md4">
+                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
+                    </div>
+                    <div className="col col-md4">
+                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
+                    </div>
+                    <div className="col col-md4">
+                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col col-md4">
+                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
+                    </div>
+                    <div className="col col-md4">
+                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
+                    </div>
+                    <div className="col col-md4">
+                        <NewsItem title="cricket news" description="sample description" imageUrl={imageUrl} />
+                    </div>
+                </div>
+              
+
+            </div>
+        </>
+    )
 }
--- newsx/src/components/NewsItem.jsx
+++ newsx/src/components/NewsItem.jsx
@@ -4,11 +4,11 @@ export default function NewsItem(props) {
     return (
         <>
             <div className="card" style={{width: "18rem"}}>
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={props.imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.description}</p>
-                    <a href="/" className="btn btn-primary">View Detailed</a>
+                    <a href="/" className="btn btn-sm btn-primary">Read More</a>
                 </div>
             </div>
         </>
