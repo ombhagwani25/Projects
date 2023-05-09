2e1a18b8ec93539edd78a5ca9c733cf3e1c78849
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-09 23:17:19 +0530

    Update app.js

--- Todo using EJS/app.js
+++ Todo using EJS/app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express();
 
+//  using ejs 
+app.set('view engine' , 'ejs');
+
 app.get("/", (req,res)=> {
     res.send("Hello");
 })
