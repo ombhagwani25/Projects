36236ec8e4ab76305de2aed00f5774d77ac387e8
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-13 19:15:17 +0530

    EJS variables

--- Todo using EJS/app.js
+++ Todo using EJS/app.js
@@ -1,12 +1,26 @@
+
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express();
 
-//  using ejs 
-app.set('view engine' , 'ejs');
 
-app.get("/", (req,res)=> {
-    res.send("Hello");
+
+//  using ejs 
+app.set('view engine' ,'ejs');
+app.get("/", (req, res) => {
+    
+    const today = new Date();
+    var currDay = today.getDay();
+    var dayString = "";
+    if (currDay == 6 || currDay == 0) {
+        dayString = "weekend"
+    } else {
+        dayString = "weekday"
+    }
+    //   res.sendFile(__dirname + "/index.html")
+    // using res.render() , that supports ejs
+    res.render('list' , {'kindOfDay' : dayString})
+
 })
 
 app.listen(3000);
