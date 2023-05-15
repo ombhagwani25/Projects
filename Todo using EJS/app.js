4c6e0b0c90d1ffcb31999497cdbad7d16a07f642
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-15 14:57:10 +0530

    using .toLocaleDateString in JS
    using tolocaledatestring in js to get the current day and date in ejs.

--- Todo using EJS/app.js
+++ Todo using EJS/app.js
@@ -3,29 +3,35 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express();
 
-
-
 //  using ejs 
 app.set('view engine' ,'ejs');
+
+app.use(bodyParser.urlencoded({extended : true}))
+
+
 app.get("/", (req, res) => {
     
     const today = new Date();
-
+    
     // another cool method of getting days using JS
     var options =  {
         weekday : "long",
         day : "numeric",
         month : "long"
     };
-
-    var day = today.toLocaleDateString("en-US",options);
-    console.log(day);
-
-  
+    
+    var day = today.toLocaleDateString("en-US",options);  
     res.render('list' , {'kindOfDay' : day,
-
+    
 })
 
 })
+
+app.post("/" , (req,res)=> {
+    var itemName = req.body.itemName;
+    console.log(itemName);
+    res.send("Donee")
+})
+
 
 app.listen(3000);
