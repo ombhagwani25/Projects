22ee7541dd5496049f3e0fcbb4b2d5e4a362713c
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-18 13:36:48 +0530

    css

--- Todo using EJS/app.js
+++ Todo using EJS/app.js
@@ -8,6 +8,8 @@ app.set('view engine' ,'ejs');
 
 app.use(bodyParser.urlencoded({extended : true}))
 
+app.use(express.static("public"));
+
 var items = ["Buy Food" , "Cook Food", "Eat Food"];
 
 app.get("/", (req, res) => {
