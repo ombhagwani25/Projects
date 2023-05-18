612236fd12759b1fdd6c414f4f0106a85bc2e07b
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-18 17:35:15 +0530

    fixed work auto-redirecting to "/" dir

--- Todo using EJS/app.js
+++ Todo using EJS/app.js
@@ -4,44 +4,49 @@ const bodyParser = require("body-parser")
 const app = express();
 
 //  using ejs 
-app.set('view engine' ,'ejs');
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static("public"));
 
-let items = ["Buy Food" , "Cook Food", "Eat Food"];
-let workItems = []; 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", (req, res) => {
-    
+
     const today = new Date();
-    
+
     // another cool method of getting days using JS
-    var options =  {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     };
-    
-    var day = today.toLocaleDateString("en-US",options);  
-    res.render('list' , {listTitle : day  , newListItem : items})
-console.log("error commit !! no changes");
+
+    var day = today.toLocaleDateString("en-US", options);
+    res.render('list', { listTitle: day, newListItem: items })
+    console.log("error commit !! no changes");
 
 })
 
-app.post("/" , (req,res)=> {
-     var item = req.body.itemName;
-     items.push(item);
-    res.redirect("/");
+app.post("/", (req, res) => {
+    var currItem = req.body.itemName;
+    if (req.body.submitButton === "Work") {
+        workItems.push(currItem);
+        res.redirect("/work");
+    } else {
+        items.push(currItem);
+        res.redirect("/");
+    }
 })
 
 // targeting the work element
-app.get("/work" , (req,res)=> {
-    res.render("list" , {listTitle : "Work" , newListItem : workItems})
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work", newListItem: workItems })
 })
 
-app.post("/work" , (req,res)=> {
+app.post("/work", (req, res) => {
     let currItem = req.body.newItem;
     workItems.push(currItem);
     res.redirect("/work");
