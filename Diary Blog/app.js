9ec60576ddfc7b4202ac8c7324f3ef31ab34baf2
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-22 14:56:47 +0530

    Challenge 5 ,6  and 7 combined

--- Diary Blog/app.js
+++ Diary Blog/app.js
@@ -30,6 +30,10 @@ app.get("/contact" , (req,res) => {
   res.render(__dirname + "/views/contact.ejs" , {contactPara : contactContent});
 })
 
+// compose page
+app.get("/compose" , (req,res) => {
+  res.render(__dirname + "/views/compose.ejs" , {});
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
