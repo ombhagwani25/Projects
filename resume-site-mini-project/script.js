6ec08828c08f1dd56c37e2ef849a30bae30f9610
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-05-27 13:33:11 +0530

    modifed script , new method of retrieving data

--- resume-site-mini-project/script.js
+++ resume-site-mini-project/script.js
@@ -1,49 +1,44 @@
-
-
 function parseURLParams(url) {
-    var queryStart = url.indexOf("?") + 1,
-        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
-        query = url.slice(queryStart, queryEnd - 1),
-        pairs = query.replace(/\+/g, " ").split("&"),
-        params = {}, i, n, v, nv;
+  var queryStart = url.indexOf("?") + 1,
+    queryEnd = url.indexOf("#") + 1 || url.length + 1,
+    query = url.slice(queryStart, queryEnd - 1),
+    pairs = query.replace(/\+/g, " ").split("&"),
+    params = {},
+    i,
+    n,
+    v,
+    nv;
 
-    if (query === url || query === "") return;
+  if (query === url || query === "") return;
 
-    for (i = 0; i < pairs.length; i++) {
-        nv = pairs[i].split("=", 2);
-        n = decodeURIComponent(nv[0]);
-        v = decodeURIComponent(nv[1]);
+  for (i = 0; i < pairs.length; i++) {
+    nv = pairs[i].split("=", 2);
+    n = decodeURIComponent(nv[0]);
+    v = decodeURIComponent(nv[1]);
 
-        if (!params.hasOwnProperty(n)) params[n] = [];
-        params[n].push(nv.length === 2 ? v : null);
-    }
+    if (!params.hasOwnProperty(n)) params[n] = [];
+    params[n].push(nv.length === 2 ? v : null);
+  }
 
-   
-    let displayString = '';
-    for (const key in params) {
-      if (params.hasOwnProperty(key)) {
-        displayString += key + ': ';
-    
-        if (Array.isArray(params[key])) {
-          const values = params[key].join(', ');
-          displayString += values;
-        } else {
-          displayString += params[key];
-        }
-    
-        displayString += '<br>';
+  let resumeHTML = '<div class="resume">';
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      resumeHTML += `<div class="resume-entry"><span class="resume-key">${key}:</span>`;
+
+      if (Array.isArray(params[key])) {
+        const values = params[key].join(', ');
+        resumeHTML += `<span class="resume-values">${values}</span>`;
+      } else {
+        resumeHTML += `<span class="resume-values">${params[key]}</span>`;
       }
+
+      resumeHTML += '</div>';
     }
-    
-    document.getElementById('displayDetails').innerHTML = displayString;
+  }
+  resumeHTML += '</div>';
 
-
-
-document.getElementById('displayDetails').innerHTML = displayString;
-
-
-     
+  document.getElementById('displayDetails').innerHTML = resumeHTML;
 }
-const URL = document.URL;
 
+const URL = document.URL;
 parseURLParams(URL);
