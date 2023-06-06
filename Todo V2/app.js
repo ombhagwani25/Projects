060a5029a6f6c8b0d5ad009b0525e490a70fd5e9
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-06-06 13:23:06 +0530

    Update app.js

--- Todo V2/app.js
+++ Todo V2/app.js
@@ -9,7 +9,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser : true});
+
 
 const itemSchema = new mongoose.Schema({
   name: String
