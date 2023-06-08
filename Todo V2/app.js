9a0ae6f82ffaa2cbf6e0eb4f4fcfe98ad2017174
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-06-09 02:06:41 +0530

    Update app.js

--- Todo V2/app.js
+++ Todo V2/app.js
@@ -1,81 +1,44 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
 
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
-mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser : true});
-
-
-const itemSchema = new mongoose.Schema({
-  name: String
-});
-
-const Item = mongoose.model("Item", itemSchema);
-
-const item1 = new Item({
-  name: "Welcome to your TodoList!"
-});
-
-const item2 = new Item({
-  name: "Hit the + icon to add a new item"
-});
-
-const item3 = new Item({
-  name: "<-- Touch this to delete an item"
-});
-
-const defaultArr = [item1, item2, item3];
-
-// Uncomment the following line if you want to insert default items every time the server starts
-// Item.insertMany(defaultArr);
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.get("/", function(req, res) {
-  Item.find({}, function(err, items) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("list", { listTitle: "Today", newListItems: items });
-    }
-  });
+
+const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
+
 });
 
-app.post("/", function(req, res) {
-  const itemName = req.body.newItem;
-  const listName = req.body.list;
+app.post("/", function(req, res){
 
-  const item = new Item({
-    name: itemName
-  });
+  const item = req.body.newItem;
 
-  if (listName === "Work") {
-    // Save the item to the database
-    item.save();
+  if (req.body.list === "Work") {
+    workItems.push(item);
     res.redirect("/work");
   } else {
-    // Save the item to the database
-    item.save();
+    items.push(item);
     res.redirect("/");
   }
 });
 
-app.get("/work", function(req, res) {
-  Item.find({}, function(err, items) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("list", { listTitle: "Work List", newListItems: items });
-    }
-  });
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
-app.get("/about", function(req, res) {
+app.get("/about", function(req, res){
   res.render("about");
 });
 
