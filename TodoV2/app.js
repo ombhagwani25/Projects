f839323a76bf8fb83d686d5a267cab809d10fec9
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-06-12 15:55:47 +0530

    Data now Receiving from Mongo DB

--- TodoV2/app.js
+++ TodoV2/app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
-const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true })
+// const mongoose = require("mongoose")
+// mongoose.connect("mongodb://localhost:27017/test")
 
 const app = express();
 
@@ -17,18 +17,33 @@ app.use(express.static("public"));
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
-const Item = mongoose.model("Item", { name: String })
 
-app.get("/", function (req, res) {
-  const day = date.getDate();
-  Item.find({}, 'name')
-    .then((items) => {
-      res.render("list", { listTitle: day, newListItems: items });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+
+
+app.get("/", async function (req, res) {
+ 
+
+ const itemArray = []
+
+  const data = await Item.find().then((temp) => {
+    temp.map(i => {
+      itemArray.push(i.name);
+      console.log(i.name);
     })
-    .catch((error) => {
-      console.error('Error retrieving items:', error);
-      res.render("list", { listTitle: day, newListItems: [] }); // Render an empty list in case of an error
-    });
+    res.render("list", { listTitle: "day", newListItems: itemArray });
+  })
+
+
+
+  
 });
 
 
