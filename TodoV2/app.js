5efdf08b36ca218cef8602aa4c964654b24e76b9
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-06-13 15:32:42 +0530

    Update app.js

--- TodoV2/app.js
+++ TodoV2/app.js
@@ -47,18 +47,23 @@ app.get("/", async function (req, res) {
 });
 
 
-app.post("/", function (req, res) {
+// app.post("/", function (req, res) {
 
-  const item = req.body.newItem;
+//   const item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
+//   if (req.body.list === "Work") {
+//     workItems.push(item);
+//     res.redirect("/work");
+//   } else {
+//     items.push(item);
+//     res.redirect("/");
+//   }
+// });
+
+app.post("/" , (req ,res)=> {
+  const currItem = req.body.newItem;
+   
+})
 
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
