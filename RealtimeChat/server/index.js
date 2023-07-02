aef2a12291b4f82af82d9512be4186e55dbdcc89
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-07-02 21:30:47 +0530

    setup mongoose connection

--- RealtimeChat/server/index.js
+++ RealtimeChat/server/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const mongoose = require('mongoose');
+
 
 app.use(express.json())
 app.use(cors())
@@ -9,7 +11,17 @@ app.use(cors())
 
 
 const port = process.env.PORT  || 5000;
+const uri = process.env.ATLAS_URI;
+
 app.listen(port, (req,res)=> {
     console.log(`Server is running on port ${port}`);
 
 });
+
+mongoose.connect(uri , {useNewUrlParser : true , useUnifiedTopology: true}).then (() =>  {
+    console.log("MongoDB database connection established successfully");
+}).catch(err => {
+    console.log("Mongo DB Connection failed, err : ",err.message);
+});
+
+
