b7e131038cba2098942d71db5a96762333578faf
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-07-03 22:32:04 +0530

    routing in forms

--- express-form-project/index.js
+++ express-form-project/index.js
@@ -9,10 +9,19 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'public', 'views'));
-    
+
 app.get("/", (req,res)=> {
     res.render('index');
 
+})
+
+
+app.post("/signup" , (req,res)=> {
+    res.send("singup page backend")
+})
+
+app.post("/signin" ,(req,res)=> {
+    res.send("signin page backend");
 })
 
 app.listen(3000);
