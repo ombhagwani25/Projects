9489fb0882f152c312383ccb27db7a5537a61f40
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-07-04 19:05:04 +0530

    fixed mongoose cluster connection

--- RealtimeChat/server/index.js
+++ RealtimeChat/server/index.js
@@ -3,8 +3,12 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
+
+
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
@@ -12,9 +16,9 @@ const uri = process.env.ATLAS_URI;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+console.log('URI:', uri);
 
-mongoose
-  .connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to the database!");
   })
