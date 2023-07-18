58712d606d1c19aa3eb53b4b3eb7afa5961505b2
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-07-18 13:10:00 +0530

    added findUser and loginUser route to the controller

--- RealtimeChat/server/Controllers/userController.js
+++ RealtimeChat/server/Controllers/userController.js
@@ -17,11 +17,11 @@ const registerUser = async (req, res) => {
 
     if(user) return res.status(400).json("User already exists");
 
-    // if(!name || !email || !password) return res.status(400).json("Please enter all fields");
+    if(!name || !email || !password) return res.status(400).json("Please enter all fields");
 
-    // if(validator.isEmail(email)) return res.status(400).json("Please enter a valid email");
+    if(validator.isEmail(email)) return res.status(400).json("Please enter a valid email");
 
-    // if(validator.isStrongPassword(password)) return res.status(400).json("Please enter a strong password");
+    if(validator.isStrongPassword(password)) return res.status(400).json("Please enter a strong password");
 
     user = new userModel({name , email, password});
 
@@ -37,4 +37,37 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports = {registerUser};
+const loginUser = async(req,res)=> {
+    const {email, password} = req.body;
+    try {
+        let user = await userModel.findOne({email});
+        if(!user) return res.json("User does not exist");
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid) return res.status(400).json("Password is incorrect...");
+
+        // now user and password are correct
+        const token = createToken(user._id);
+        res.status(200).json({_id : user._id , name : user.name , email ,token});
+
+
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json("Internal server error");
+    }
+}
+
+const findUser = async (req,res) => {
+    const userId = req.params.userId;
+    try {
+        const user = await userModel.findById(userId);
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+module.exports = {registerUser, loginUser,  findUser};
--- RealtimeChat/server/Routes/userRoute.js
+++ RealtimeChat/server/Routes/userRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const {registerUser} = require("../Controllers/userController");
+const {registerUser , loginUser, findUser} = require("../Controllers/userController");
 
 
 const router = express.Router();
 
 
 router.post("/register" , registerUser);
+router.post("/login" , loginUser );
+router.get("/find/:userId" , findUser);
 
-module.exports = router;
+
+
+module.exports = router;    
