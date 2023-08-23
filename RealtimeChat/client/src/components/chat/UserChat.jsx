de767cd6aaedaa2babb6be3773d9a70538dcde4f
Om Bhagwani <119593886+ombhagwani25@users.noreply.github.com>
2023-08-23 19:16:23 +0530

    Chat Box Designed UI

--- RealtimeChat/client/src/components/chat/UserChat.jsx
+++ RealtimeChat/client/src/components/chat/UserChat.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useFetchRecipient } from '../../hooks/useFetchRecipient'
 import { Stack } from 'react-bootstrap'
-
+import avatar from "../../assets/avatar.svg"
 export default function UserChat({chat, user}) {
 
     const {recipientUser} = useFetchRecipient(chat, user)
@@ -10,10 +10,17 @@ export default function UserChat({chat, user}) {
     <Stack direction='horizontal' gap={3} className='user-card align-items-center p-2 justify-content-between'>
         <div className="d-flex">
             <div className="me-2">
-                Avatar
+                <img src={avatar} height="35px" alt="Avatar" />
             </div>
+            <div className="text-content">
             <div className="name">{recipientUser?.name}</div>
             <div className="text">Texts</div>
+            </div>
+        </div>
+        <div className="d-flex flex-column align-items-end">
+          <div className="date">23/08/2023</div>
+          <div className="this-user-notifications">2</div>
+          <span className="user-online"></span>
         </div>
     </Stack>
   )
--- RealtimeChat/client/src/pages/Chat.jsx
+++ RealtimeChat/client/src/pages/Chat.jsx
@@ -1,37 +1,27 @@
-import React, { useContext } from 'react'
-import { ChatContext } from '../context/ChatContext'
-import { AuthContext } from '../context/AuthContext'
-import { Container, Stack } from "react-bootstrap"
+import React, { useContext } from 'react';
+import { ChatContext } from '../context/ChatContext';
+import { AuthContext } from '../context/AuthContext';
+import { Container, Stack } from 'react-bootstrap';
 import UserChat from '../components/chat/UserChat';
 
 export default function Chat() {
+  const { user } = useContext(AuthContext);
+  const { userChats = [], isUserChatsLoading, userChatsError } = useContext(ChatContext);
 
-  const {user} = useContext(AuthContext)
-  const { userChats, isUserChatsLoading, userChatsError } = useContext(ChatContext)
-
-  console.log("user chats : ", userChats);
   return (
-    <>
-      <Container>
-        {userChats?.length < 1 ?
-          null :
-          <Stack direction='horizontal' gap={4} className='align-items-start'>
-            <Stack className='messages-box flex-grow-0 pe-3' gap={3}>
-              {isUserChatsLoading && <p>Loading Chats...</p>}
+    <Container>
+      <Stack direction='horizontal' gap={4} className='align-items-start'>
+        <Stack className='messages-box flex-grow-0 pe-3' gap={3}>
+          {isUserChatsLoading && <p>Loading Chats...</p>}
 
-              {userChats.map((chat,index)=> {
-                return (
-                  <div key ={index}>
-                    <UserChat chat={chat} user={user} />
-                  </div>
-                )
-              })}
-            </Stack>
-            <Stack>ChatBox</Stack>
-          </Stack>
-
-        }
-      </Container>
-    </>
-  )
+          {userChats.map((chat, index) => (
+            <div key={index}>
+              <UserChat chat={chat} user={user} />
+            </div>
+          ))}
+        </Stack>
+        <Stack>ChatBox</Stack>
+      </Stack>
+    </Container>
+  );
 }
